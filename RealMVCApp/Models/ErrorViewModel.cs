978892b28768c1965571d3f0a0ce352339887899
Blinde16 <blakejlinde@gmail.com>
2025-02-10 13:01:36 -0700

    Add project files.

---
+++ RealMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
